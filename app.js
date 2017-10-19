c4087e96ad6c1416d3a6481bbf74e66e830a2fa5
dhoncharenko <dhoncharenko@lohika.com>
2017-10-19 12:02:33 +0300

    slider done

--- app.js
+++ app.js
@@ -4,31 +4,29 @@ function q(selector, parent) {
 }
 
 var slider = q('.slider__container');
-var slides = q('.slider__container > li');
+var slides = q('.slider__container > img');
 
-
-// prapare slides
-var pics = [
+// Prepare slides
+[
     'pic1.jpg',
-    'pic2.jpg'
-];
-
-pics.forEach((url, index) => {
-    var pic = q('img', slides[index]);
-    pic.src = url;
+    'pic2.jpg',
+    'pic3.jpg',
+    'pic1.jpg'
+]
+.map(path => 'images/slider/' + path)
+.forEach((url, index) => {
+    slides[index].src = url;
 });
 
+// Run slide show
 var i = 0;
-
-
+setActiveSlide(i);
 setInterval(() => {
-    if (i >= slides.length - 1) {
-        i = 0;
-    } else {
-        i++;
-    }
-    slides.forEach(s => s.classList.remove('active'));
-    
-    slides[i].classList.add('active');
-    
+    (i >= slides.length - 1) ? i = 0 : i++;
+    setActiveSlide(i);
 }, 3000);
+
+function setActiveSlide(i) {
+    slides.forEach(s => s.classList.remove('active'));
+    slides[i].classList.add('active');
+}
